4a3011ef9c1b708a8bdbd4a44f9da21423a03ec2
Roshni0511 <roshnichauhan051@gmail.com>
2025-06-02 14:25:04 +0530

    add

--- jaipur_gemological_lab/src/App.js
+++ jaipur_gemological_lab/src/App.js
@@ -1,12 +1,13 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home';
 
 function App() {
   return (
   <>
             <Routes>
-              <Route />
+               <Route path='/' element={<Home />}/>
             </Routes>
   </>
   );
